109b8de29828e98d98c51931f6f6583432a378a1
John Van Note <johnlvannote@gmail.com>
2018-07-25 22:12:17 -0400

    Adding experience section

---
+++ src/main/java/com/jvn/resume/section/ExperienceSection.java
@@ -0,0 +1,34 @@
+package com.jvn.resume.section;
+
+import com.google.common.base.MoreObjects;
+import com.jvn.Experience;
+import java.util.List;
+
+public class ExperienceSection extends AbstractTitledSection {
+
+  private List<Experience> experiences;
+
+  public ExperienceSection(List<Experience> experiences) {
+    this.setExperiences(experiences);
+  }
+
+  public String valueOfTitle() {
+    return "Experience";
+  }
+
+  public List<Experience> getExperiences() {
+    return experiences;
+  }
+
+  public void setExperiences(List<Experience> experiences) {
+    this.experiences = experiences;
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("experiences", experiences)
+        .omitNullValues()
+        .toString();
+  }
+}
