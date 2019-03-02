08ca18670547746dad7d5816d9c6dfbe2e01c2a5
John Van Note <johnlvannote@gmail.com>
2019-03-02 09:24:36 -0500

    Added unit test

---
+++ src/test/java/com/jvn/resume/section/ExperienceSection_UnitTest.java
@@ -0,0 +1,14 @@
+package com.jvn.resume.section;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExperienceSection_UnitTest {
+
+  @Test
+  public void getTitle_NoState_IsEducation() {
+    ExperienceSection section = new ExperienceSection();
+    Assert.assertEquals(section.getTitle(), "Experience");
+  }
+
+}
