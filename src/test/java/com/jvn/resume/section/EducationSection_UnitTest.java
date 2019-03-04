fd4ec1a6f12dbf2a9c5e87eb0012620a8c9477c6
John Van Note <johnlvannote@gmail.com>
2019-03-03 21:58:41 -0500

    Added unit tests

--- src/test/java/com/jvn/resume/section/EducationSection_UnitTest.java
+++ src/test/java/com/jvn/resume/section/EducationSection_UnitTest.java
@@ -1,14 +1,30 @@
 package com.jvn.resume.section;
 
+import com.google.common.collect.ImmutableList;
+import com.jvn.resume.item.Education;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import test.util.ToStringUtil;
 
 public class EducationSection_UnitTest {
 
   @Test
-  public void getTitle_AnyoState_IsEducation() {
+  public void getTitle_AnyState_IsEducation() {
     EducationSection section = new EducationSection();
     Assert.assertEquals(section.getTitle(), "Education");
+  }
+
+  @Test
+  public void toString_AnyState_PrintsFields() {
+    Education school1 = Education.builder().degree("Degree").build();
+    Education school2 = Education.builder().gpa("11.0").build();
+
+    EducationSection section = new EducationSection();
+    section.setSchools(ImmutableList.of(school1, school2));
+
+    ToStringUtil.assertToStringContains(section.toString(),
+        "Degree", "11.0", "Education"
+    );
   }
 
 }
